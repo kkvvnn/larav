ae4624f07cc0d4362a828e28c8ef1a34c994b995
kkvvnn <kkvvnn89@gmail.com>
2019-12-30 23:43:47 +0500

    unknown commit!

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel ++++++
+                    kkvvnn
                 </div>
 
                 <div class="links">
